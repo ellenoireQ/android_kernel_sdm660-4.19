3138125d75f2250ea86c857fe44903123e87422c
deflamingq o <sukairo71@gmail.com>
2024-08-10 11:33:11 +0000

    sched: core: remove rq->load.weight

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4118,7 +4118,6 @@ void get_iowait_load(unsigned long *nr_waiters, unsigned long *load)
 {
 	struct rq *rq = this_rq();
 	*nr_waiters = atomic_read(&rq->nr_iowait);
-	*load = rq->load.weight;
 }
 
 #ifdef CONFIG_SMP
